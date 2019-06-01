2a07d66a1987cbc6bd618f07066207625120d7df
Jasmine Chen <jasmine1226@gmail.com>
2019-06-01 02:41:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,21 +3,21 @@ class Song
   @@song_count = 0
   @@artists = []
   @@genres = []
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@song_count +=1
-    
+
     if @@artists.include?(artist) == false
       @@artist << artist
     end
-    
+
     if @@genres.include?(genre) == false
       @@genres << genre
     end
-  end 
+  end
 
   def self.count
     @@album_count
