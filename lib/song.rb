be1fba8b200e0404bd14f11c6a0aca34c3e57328
Jasmine Chen <jasmine1226@gmail.com>
2019-06-01 02:41:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@song_count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@song_count +=1
+    
+    if @@artists.include?(artist) == false
+      @@artist << artist
+    end
+    
+    if @@genres.include?(genre) == false
+      @@genres << genre
+    end
+  end 
+
+  def self.count
+    @@album_count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+end
